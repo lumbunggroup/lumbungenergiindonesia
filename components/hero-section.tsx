10a9766922332d4ca230edc64d8dfb8693fc8937
julianromli <faizintfd@gmail.com>
2025-10-07 14:31:43 +0700

    feat: simplify energy procurement description in hero section

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -16,7 +16,7 @@ export default function HeroSection() {
                         <div className="relative mx-auto flex max-w-7xl flex-col px-6 lg:block lg:px-12">
                             <div className="mx-auto max-w-lg text-center lg:ml-0 lg:max-w-full lg:text-left">
                                 <h1 className="mt-8 max-w-2xl text-balance text-5xl md:text-6xl lg:mt-16 xl:text-7xl text-white">Selalu Bersinergi untuk Bisnis Anda</h1>
-                                <p className="mt-8 max-w-2xl text-balance text-lg text-white">Pengadaan energi (batubara/biomassa), PLTS (Pembangkit Listrik Tenaga Surya), ME (Mekanikal & Elektrikal), TI (Teknologi Informasi), dan logistik dalam satu mitra. Fokus pada keselamatan kerja, kepatuhan, dan ketepatan eksekusi.</p>
+                                <p className="mt-8 max-w-2xl text-balance text-lg text-white">Pengadaan energi , PLTS, ME, TI, dan logistik dalam satu mitra. Fokus pada keselamatan kerja, kepatuhan, dan ketepatan eksekusi.</p>
 
                                 <div className="mt-12 flex flex-col items-center justify-center gap-2 sm:flex-row lg:justify-start">
                                     <Button
